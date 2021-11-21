7ef9a44e67610d37b0cf64fa1a5c33593954ef8b
bbggkkk <bbggkkk@naver.com>
2021-11-21 22:59:09 +0900

    window 객체에 추가

--- src/index.ts
+++ src/index.ts
@@ -31,4 +31,13 @@ import { ScrollAnimation, ScrollAnimationItem } from "./scrollAnimation";
 //     }
 // };
 
+declare global {
+    interface Window {
+        ScrollAnimation : Function;
+    }
+}
+
+if(!(typeof exports === 'object' && typeof module !== 'undefined')){
+    window.ScrollAnimation = ScrollAnimation;
+}
 console.log(new ScrollAnimation(window, '.target'));
